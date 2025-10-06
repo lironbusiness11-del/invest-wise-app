c897cd41ffb88a6e91863e3c29ba44831e96090c
lironbusiness11-del <liron.business11@gmail.com>
2025-10-06 23:34:02 +0300

    Update App.jsx with routing and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
+import LessonPage from './pages/LessonPage';
+import QuizPage from './pages/QuizPage';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Invest Wise - Investment Learning Platform</h1>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<div>Welcome to Invest Wise!</div>} />
-        </Routes>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <header className="app-header">
+          <h1>Invest Wise</h1>
+          <nav className="main-nav">
+            <Link to="/">Dashboard</Link>
+            <Link to="/lesson">Lessons</Link>
+            <Link to="/quiz">Quiz</Link>
+          </nav>
+        </header>
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<DashboardPage />} />
+            <Route path="/lesson" element={<LessonPage />} />
+            <Route path="/quiz" element={<QuizPage />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
