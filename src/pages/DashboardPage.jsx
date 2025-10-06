2dd186bbac122a8ccfab04aa75641cb737adaf23
lironbusiness11-del <liron.business11@gmail.com>
2025-10-06 23:32:27 +0300

    Create DashboardPage.jsx

---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Dashboard from '../components/Dashboard';
+
+const DashboardPage = () => {
+  return (
+    <div className="page dashboard-page">
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
