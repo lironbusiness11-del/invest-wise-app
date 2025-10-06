331afede2d34a26a26839de906762c9ad3c2ae06
lironbusiness11-del <liron.business11@gmail.com>
2025-10-06 23:33:20 +0300

    Create QuizPage.jsx

---
+++ src/pages/QuizPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Quiz from '../components/Quiz';
+
+const QuizPage = () => {
+  return (
+    <div className="page quiz-page">
+      <Quiz />
+    </div>
+  );
+};
+
+export default QuizPage;
