73ad66d91d9fc12408bf42b72d7944626e8ceef2
lironbusiness11-del <liron.business11@gmail.com>
2025-10-06 23:57:39 +0300

    Fix: Add App component to BrowserRouter in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './App'; // If file is App.jsx, change to './App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
