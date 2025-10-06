e4f7650581c8f670da4b38d2d822d45d162d7cb7
lironbusiness11-del <liron.business11@gmail.com>
2025-10-06 23:32:53 +0300

    Create LessonPage.jsx

---
+++ src/pages/LessonPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Lesson from '../components/Lesson';
+
+const LessonPage = () => {
+  return (
+    <div className="page lesson-page">
+      <Lesson />
+    </div>
+  );
+};
+
+export default LessonPage;
