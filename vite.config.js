2e07339d16e25ea2b86a89681c324c80e2151345
lironbusiness11-del <liron.business11@gmail.com>
2025-10-08 12:40:37 +0300

    fix build output directory

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
+  base: '/',
   build: {
-    outDir: 'build'
+    outDir: 'dist' // 👈 this tells Vite to build into dist/
   }
 })
+
